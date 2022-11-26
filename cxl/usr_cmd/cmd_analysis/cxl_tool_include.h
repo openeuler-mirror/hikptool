43e127d0efb0e2a3d371ca350ef69c9cf1af3097
veega2022 <zhuweijia@huawei.com>
2022-11-26 16:20:56 +0800

    support querying CXL module information
    including the I/O link, MEM link, CPA/DL register, MEMAR_BAR, and RCRB information
    
    Signed-off-by: hesiyuan <hesiyuan4@huawei.com>
    (cherry picked from commit 99eb7b4e6a07a14a00e6cb3d239e65d5e81dcad7)

---
+++ cxl/usr_cmd/cmd_analysis/cxl_tool_include.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+#ifndef __CXL_TOOL_INCLUDE_H_
+#define __CXL_TOOL_INCLUDE_H_
+
+#include <stdint.h>
+
+struct tool_cxl_cmd {
+	uint32_t cmd_type;
+	uint32_t port_id;
+};
+
+#endif

9af0bf084d8da11bac743edcca866cdaad71c80e
veega <zhuweijia@huawei.com>
2023-05-16 10:39:39 +0800

    hikptool: delete redundant copyright notices
    Delete redundant copyright notices in hikp_ub_link.h
    
    Signed-off-by: veega <zhuweijia@huawei.com>

--- net/ub/ub_link/hikp_ub_link.h
+++ net/ub/ub_link/hikp_ub_link.h
@@ -11,19 +11,6 @@
  * See the Mulan PSL v2 for more details.
  */
 
-/*
- * Copyright (c) 2023 Hisilicon Technologies Co., Ltd.
- * Hikptool is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
- *          http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- *
- * See the Mulan PSL v2 for more details.
- */
-
 #ifndef HIKP_UB_LINK_H
 #define HIKP_UB_LINK_H
 

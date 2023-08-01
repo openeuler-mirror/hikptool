520b9531e154cadbca678203df6a7eb384a4afbb
Junxian Huang <huangjunxian6@hisilicon.com>
2023-08-01 17:41:24 +0800

    hikptool/roce: Add ext query for roce_caep cmd
    Add ext query for roce_caep cmd. To query these ext registers, add
    -e to the query cmd.
    
    Example:
    hikptool roce_caep -i eth1 -e
    
    Signed-off-by: Junxian Huang <huangjunxian6@hisilicon.com>

--- net/roce/roce_caep/hikp_roce_caep.h
+++ net/roce/roce_caep/hikp_roce_caep.h
@@ -21,10 +21,16 @@
 
 struct cmd_roce_caep_param_t {
 	struct tool_target target;
+	uint32_t sub_cmd;
 };
 
 struct roce_caep_req_param {
 	struct bdf_t bdf;
+};
+
+struct roce_caep_req_param_ext {
+	struct roce_caep_req_param origin_param;
+	uint32_t block_id;
 };
 
 struct roce_caep_res {
@@ -35,6 +41,11 @@ struct roce_caep_res {
 struct roce_caep_res_param {
 	uint32_t total_block_num;
 	struct roce_caep_res reg_data;
+};
+
+enum roce_caep_cmd_type {
+	CAEP_ORIGIN = 0,
+	CAEP_EXT,
 };
 
 #endif /* __HIKP_ROCE_CAEP_H__ */

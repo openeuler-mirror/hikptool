openeuler-mirror/hikptool

commit 392c269be165f687f5fbeb14279aece562152f56

support querying RoCE module information
querying information including flow control algorithm parameters,
packet sending and receiving, and internal sub-module status.

Signed-off-by: shushengming <shushengming1@huawei.com>

net/roce/roce_caep/hikp_roce_caep.c
@@ -1,0 +1,103 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include "hikp_roce_caep.h"
+
+static struct cmd_roce_caep_param_t g_roce_caep_param_t = { 0 };
+
+static int hikp_roce_caep_help(struct major_cmd_ctrl *self, const char *argv)
+{
+	printf("\n  Usage: %s %s\n", self->cmd_ptr->name, "-i <interface>\n");
+	printf("\n         %s\n", self->cmd_ptr->help_info);
+	printf("  Options:\n\n");
+	printf("    %s, %-25s %s\n", "-h", "--help", "display this help and exit");
+	printf("    %s, %-25s %s\n", "-i", "--interface=<interface>", "device target, e.g. eth0");
+	printf("\n");
+
+	return 0;
+}
+
+static int hikp_roce_caep_target(struct major_cmd_ctrl *self, const char *argv)
+{
+	self->err_no = tool_check_and_get_valid_bdf_id(argv, &(g_roce_caep_param_t.target));
+	if (self->err_no !=  0) {
+		snprintf(self->err_str, sizeof(self->err_str), "Unknown device %s.", argv);
+		return self->err_no;
+	}
+
+	return 0;
+}
+
+static int hikp_roce_caep_get_data(struct hikp_cmd_ret **cmd_ret,
+				   struct roce_caep_req_param req_data)
+{
+	struct hikp_cmd_header req_header = { 0 };
+
+	hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_CAEP_CMD, 0);
+	*cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+	if (*cmd_ret == NULL) {
+		printf("hikptool roce_caep cmd_ret malloc failed\n");
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static void hikp_roce_caep_print(uint32_t total_block_num,
+				 const uint32_t *offset, const uint32_t *data)
+{
+	uint32_t i;
+
+	printf("**************CAEP INFO*************\n");
+	for (i = 0; i < total_block_num; i++)
+		printf("[0x%08X] : 0x%08X\n", offset[i], data[i]);
+	printf("************************************\n");
+}
+
+static void hikp_roce_caep_execute(struct major_cmd_ctrl *self)
+{
+	struct roce_caep_res_param *roce_caep_res;
+	struct roce_caep_req_param req_data;
+	struct hikp_cmd_ret *cmd_ret;
+	int ret;
+
+	req_data.bdf = g_roce_caep_param_t.target.bdf;
+	ret = hikp_roce_caep_get_data(&cmd_ret, req_data);
+	if (ret < 0) {
+		self->err_no = ret;
+		return;
+	} else if (cmd_ret->status != 0) {
+		printf("hikptool roce_caep read data failed\n");
+		goto exec_error;
+	}
+	roce_caep_res = (struct roce_caep_res_param *)cmd_ret->rsp_data;
+	hikp_roce_caep_print(roce_caep_res->total_block_num,
+			     roce_caep_res->reg_data.offset, roce_caep_res->reg_data.data);
+
+exec_error:
+	free(cmd_ret);
+	cmd_ret = NULL;
+}
+
+static void cmd_roce_caep_init(void)
+{
+	struct major_cmd_ctrl *major_cmd = get_major_cmd();
+
+	major_cmd->option_count = 0;
+	major_cmd->execute = hikp_roce_caep_execute;
+
+	cmd_option_register("-h", "--help", false, hikp_roce_caep_help);
+	cmd_option_register("-i", "--interface", true, hikp_roce_caep_target);
+}
+
+HIKP_CMD_DECLARE("roce_caep", "get roce_caep registers information", cmd_roce_caep_init);

net/roce/roce_pkt/hikp_roce_pkt.c
@@ -1,0 +1,107 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include "hikp_roce_pkt.h"
+
+static struct cmd_roce_pkt_param_t g_roce_pkt_param_t = { 0 };
+
+static int hikp_roce_pkt_help(struct major_cmd_ctrl *self, const char *argv)
+{
+	printf("\n  Usage: %s %s\n", self->cmd_ptr->name, "-i <interface>\n");
+	printf("\n         %s\n", self->cmd_ptr->help_info);
+	printf("  Options:\n\n");
+	printf("    %s, %-25s %s\n", "-h", "--help", "display this help and exit");
+	printf("    %s, %-25s %s\n", "-i", "--interface=<interface>", "device target, e.g. eth0");
+	printf("    %s, %-25s %s\n", "-c", "--clear=<clear>", "clear param count registers");
+	printf("\n");
+
+	return 0;
+}
+
+static int hikp_roce_pkt_target(struct major_cmd_ctrl *self, const char *argv)
+{
+	self->err_no = tool_check_and_get_valid_bdf_id(argv, &(g_roce_pkt_param_t.target));
+	if (self->err_no !=  0) {
+		snprintf(self->err_str, sizeof(self->err_str), "Unknown device %s.", argv);
+		return self->err_no;
+	}
+
+	return 0;
+}
+
+static int hikp_roce_pkt_clear_set(struct major_cmd_ctrl *self, const char *argv)
+{
+	g_roce_pkt_param_t.reset_flag = 1;
+	return 0;
+}
+
+static int hikp_roce_pkt_get_data(struct hikp_cmd_ret **cmd_ret, struct roce_pkt_req_param req_data)
+{
+	struct hikp_cmd_header req_header = { 0 };
+
+	hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_PKT_CMD, 0);
+	*cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+	if (*cmd_ret == NULL) {
+		printf("hikptool roce_pkt cmd_ret malloc failed\n");
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static void hikp_roce_pkt_print(uint32_t total_block_num,
+				const uint32_t *offset, const uint32_t *data)
+{
+	uint32_t i;
+
+	printf("**************PKT INFO*************\n");
+	for (i = 0; i < total_block_num; i++)
+		printf("[0x%08X] : 0x%08X\n", offset[i], data[i]);
+	printf("***********************************\n");
+}
+
+static void hikp_roce_pkt_execute(struct major_cmd_ctrl *self)
+{
+	struct roce_pkt_req_param req_data = { 0 };
+	struct roce_pkt_res_param *roce_pkt_res;
+	struct hikp_cmd_ret *cmd_ret;
+	int ret;
+
+	req_data.bdf = g_roce_pkt_param_t.target.bdf;
+	req_data.reset_flag = g_roce_pkt_param_t.reset_flag;
+	ret = hikp_roce_pkt_get_data(&cmd_ret, req_data);
+	if (ret < 0) {
+		self->err_no = ret;
+		return;
+	}
+	roce_pkt_res = (struct roce_pkt_res_param *)cmd_ret->rsp_data;
+	hikp_roce_pkt_print(roce_pkt_res->total_block_num,
+			    roce_pkt_res->reg_data.offset, roce_pkt_res->reg_data.data);
+
+	free(cmd_ret);
+	cmd_ret = NULL;
+}
+
+static void cmd_roce_pkt_init(void)
+{
+	struct major_cmd_ctrl *major_cmd = get_major_cmd();
+
+	major_cmd->option_count = 0;
+	major_cmd->execute = hikp_roce_pkt_execute;
+
+	cmd_option_register("-h", "--help", false, hikp_roce_pkt_help);
+	cmd_option_register("-i", "--interface", true, hikp_roce_pkt_target);
+	cmd_option_register("-c", "--clear", false, hikp_roce_pkt_clear_set);
+}
+
+HIKP_CMD_DECLARE("roce_pkt", "get or clear roce_pkt registers information", cmd_roce_pkt_init);

net/roce/roce_qmm/hikp_roce_qmm.c
@@ -1,0 +1,179 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include <unistd.h>
+#include "hikp_roce_qmm.h"
+
+static struct cmd_roce_qmm_param_t g_roce_qmm_param = { 0 };
+
+static int hikp_roce_qmm_help(struct major_cmd_ctrl *self, const char *argv)
+{
+	printf("\n  Usage: %s %s\n", self->cmd_ptr->name, "-i <interface>\n");
+	printf("\n         %s\n", self->cmd_ptr->help_info);
+	printf("  Options:\n\n");
+	printf("    %s, %-25s %s\n", "-h", "--help", "display this help and exit");
+	printf("    %s, %-25s %s\n", "-i", "--interface=<interface>", "device target, e.g. eth0");
+	printf("    %s, %-25s %s\n", "-b", "--bank=<bank>",
+	       "[option]bank number, e.g. 0~7. (default 0)");
+	printf("\n");
+
+	return 0;
+}
+
+static int hikp_roce_qmm_target(struct major_cmd_ctrl *self, const char *argv)
+{
+	self->err_no = tool_check_and_get_valid_bdf_id(argv, &(g_roce_qmm_param.target));
+	if (self->err_no != 0) {
+		snprintf(self->err_str, sizeof(self->err_str), "Unknown device %s.", argv);
+		return self->err_no;
+	}
+
+	return 0;
+}
+
+static int hikp_roce_qmm_bank_get(struct major_cmd_ctrl *self, const char *argv)
+{
+	char *endptr = NULL;
+	int bank_num;
+
+	bank_num = strtol(argv, &endptr, 0);
+	if ((endptr <= argv) || (*endptr != '\0') || bank_num > QMM_BANK_NUM || bank_num < 0) {
+		snprintf(self->err_str, sizeof(self->err_str), "Invalid bank number!\n");
+		self->err_no = -EINVAL;
+		return -EINVAL;
+	}
+
+	g_roce_qmm_param.bank_id = bank_num;
+	return 0;
+}
+
+static void hikp_roce_qmm_print(struct roce_qmm_rsp_data *qmm_rsp)
+{
+	int index = 0;
+
+	while (index < qmm_rsp->reg_num) {
+		printf("0x%08X : 0x%08X\n", qmm_rsp->qmm_content[index][0],
+		       qmm_rsp->qmm_content[index][1]);
+		index++;
+	}
+	printf("***************************************\n");
+}
+
+static int hikp_roce_qmm_show_cqc(struct major_cmd_ctrl *self)
+{
+	struct roce_qmm_req_para req_data = { 0 };
+	struct roce_qmm_rsp_data *qmm_rsp = NULL;
+	struct hikp_cmd_header req_header = { 0 };
+	struct hikp_cmd_ret *cmd_ret = NULL;
+	int ret;
+
+	req_data.bdf = g_roce_qmm_param.target.bdf;
+
+	hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_QMM_CMD, QMM_SHOW_CQC);
+	cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+	ret = hikp_rsp_normal_check(cmd_ret);
+	if (ret != 0)
+		goto out;
+
+	qmm_rsp = (struct roce_qmm_rsp_data *)(cmd_ret->rsp_data);
+	printf("**************QMM CQC INFO*************\n");
+	hikp_roce_qmm_print(qmm_rsp);
+out:
+	free(cmd_ret);
+	cmd_ret = NULL;
+	return ret;
+}
+
+static int hikp_roce_qmm_show_qpc(struct major_cmd_ctrl *self)
+{
+	struct roce_qmm_req_para req_data = { 0 };
+	struct roce_qmm_rsp_data *qmm_rsp = NULL;
+	struct hikp_cmd_header req_header = { 0 };
+	struct hikp_cmd_ret *cmd_ret = NULL;
+	int ret;
+
+	req_data.bdf = g_roce_qmm_param.target.bdf;
+	req_data.bank_id = g_roce_qmm_param.bank_id;
+
+	hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_QMM_CMD, QMM_SHOW_QPC);
+	cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+	ret = hikp_rsp_normal_check(cmd_ret);
+	if (ret != 0)
+		goto out;
+
+	qmm_rsp = (struct roce_qmm_rsp_data *)(cmd_ret->rsp_data);
+	printf("**************QMM QPC INFO*************\n");
+	hikp_roce_qmm_print(qmm_rsp);
+out:
+	free(cmd_ret);
+	cmd_ret = NULL;
+	return ret;
+}
+
+static int hikp_roce_qmm_show_top(struct major_cmd_ctrl *self)
+{
+	struct roce_qmm_req_para req_data = { 0 };
+	struct roce_qmm_rsp_data *qmm_rsp = NULL;
+	struct hikp_cmd_header req_header = { 0 };
+	struct hikp_cmd_ret *cmd_ret = NULL;
+	int ret;
+
+	req_data.bdf = g_roce_qmm_param.target.bdf;
+
+	hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_QMM_CMD, QMM_SHOW_TOP);
+	cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+	ret = hikp_rsp_normal_check(cmd_ret);
+	if (ret != 0)
+		goto out;
+
+	qmm_rsp = (struct roce_qmm_rsp_data *)(cmd_ret->rsp_data);
+	printf("**************QMM TOP INFO*************\n");
+	hikp_roce_qmm_print(qmm_rsp);
+out:
+	free(cmd_ret);
+	cmd_ret = NULL;
+	return ret;
+}
+
+static void hikp_roce_qmm_execute(struct major_cmd_ctrl *self)
+{
+	int (*func[])(struct major_cmd_ctrl *self) = {
+		hikp_roce_qmm_show_cqc, hikp_roce_qmm_show_qpc, hikp_roce_qmm_show_top
+	};
+	char *function[] = {"show cqc", "show qpc", "show top"};
+	int ret;
+
+	for (int i = 0; i < HIKP_ARRAY_SIZE(func); i++) {
+		ret = func[i](self);
+		if (ret != 0) {
+			self->err_no = -EINVAL;
+			snprintf(self->err_str, sizeof(self->err_str),
+				 "roce_qmm %s function failed\n", function[i]);
+			break;
+		}
+	}
+}
+
+static void cmd_roce_qmm_init(void)
+{
+	struct major_cmd_ctrl *major_cmd = get_major_cmd();
+
+	major_cmd->option_count = 0;
+	major_cmd->execute = hikp_roce_qmm_execute;
+
+	cmd_option_register("-h", "--help", false, hikp_roce_qmm_help);
+	cmd_option_register("-i", "--interface", true, hikp_roce_qmm_target);
+	cmd_option_register("-b", "--bank", true, hikp_roce_qmm_bank_get);
+}
+
+HIKP_CMD_DECLARE("roce_qmm", "get roce_qmm registers information", cmd_roce_qmm_init);

net/roce/roce_timer/hikp_roce_timer.c
@@ -1,0 +1,174 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+#include <unistd.h>
+#include "hikp_roce_timer.h"
+
+static struct cmd_roce_timer_params g_roce_timer_param = { 0 };
+
+static int hikp_roce_timer_help(struct major_cmd_ctrl *self, const char *argv)
+{
+	printf("\n  Usage: %s %s\n", self->cmd_ptr->name, "-i <interface>\n");
+	printf("\n         %s\n", self->cmd_ptr->help_info);
+	printf("  Options:\n\n");
+	printf("    %s, %-25s %s\n", "-h", "--help", "display this help and exit");
+	printf("    %s, %-25s %s\n", "-i", "--interface=<interface>", "device target, e.g. eth0");
+	printf("    %s, %-25s %s\n", "-c", "--clear=<clear>", "clear timer registers");
+	printf("\n");
+
+	return 0;
+}
+
+static int hikp_roce_timer_target(struct major_cmd_ctrl *self, const char *argv)
+{
+	self->err_no = tool_check_and_get_valid_bdf_id(argv, &(g_roce_timer_param.target));
+	if (self->err_no != 0) {
+		snprintf(self->err_str, sizeof(self->err_str), "Unknown device %s.", argv);
+		return self->err_no;
+	}
+	return 0;
+}
+
+static int hikp_roce_timer_clear_set(struct major_cmd_ctrl *self, const char *argv)
+{
+	g_roce_timer_param.flag = ROCE_TIMER_CMD_CLEAR;
+	return 0;
+}
+
+static void hikp_roce_timer_print(struct roce_timer_rsp_data *timer_rsp)
+{
+	int index = 0;
+
+	while (index < timer_rsp->reg_num) {
+		printf("0x%08X : 0x%08X\n", timer_rsp->timer_content[index][0],
+		       timer_rsp->timer_content[index][1]);
+		index++;
+	}
+	printf("*****************************************\n");
+}
+
+static int hikp_roce_timer_show_qpc(struct major_cmd_ctrl *self)
+{
+	struct roce_timer_req_para req_data = { 0 };
+	struct roce_timer_rsp_data *timer_rsp = NULL;
+	struct hikp_cmd_header req_header = { 0 };
+	struct hikp_cmd_ret *cmd_ret = NULL;
+	int ret;
+
+	req_data.bdf = g_roce_timer_param.target.bdf;
+	if (g_roce_timer_param.flag)
+		hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_TIMER_CMD, TIMER_QPC_CLEAR);
+	else
+		hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_TIMER_CMD, TIMER_SHOW_QPC);
+
+	cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+	ret = hikp_rsp_normal_check(cmd_ret);
+	if (ret != 0)
+		goto out;
+
+	timer_rsp = (struct roce_timer_rsp_data *)(cmd_ret->rsp_data);
+	printf("**************QPC TIMER INFO*************\n");
+	hikp_roce_timer_print(timer_rsp);
+out:
+	free(cmd_ret);
+	cmd_ret = NULL;
+	return ret;
+}
+
+static int hikp_roce_timer_show_cqc(struct major_cmd_ctrl *self)
+{
+	struct roce_timer_req_para req_data = { 0 };
+	struct roce_timer_rsp_data *timer_rsp = NULL;
+	struct hikp_cmd_header req_header = { 0 };
+	struct hikp_cmd_ret *cmd_ret = NULL;
+	int ret;
+
+	req_data.bdf = g_roce_timer_param.target.bdf;
+	if (g_roce_timer_param.flag)
+		hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_TIMER_CMD, TIMER_CQC_CLEAR);
+	else
+		hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_TIMER_CMD, TIMER_SHOW_CQC);
+
+	cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+	ret = hikp_rsp_normal_check(cmd_ret);
+	if (ret != 0)
+		goto out;
+
+	timer_rsp = (struct roce_timer_rsp_data *)(cmd_ret->rsp_data);
+	printf("**************CQC TIMER INFO*************\n");
+	hikp_roce_timer_print(timer_rsp);
+out:
+	free(cmd_ret);
+	cmd_ret = NULL;
+	return ret;
+}
+
+static int hikp_roce_timer_clear(struct major_cmd_ctrl *self)
+{
+	int ret_clear_qpc;
+	int ret_clear_cqc;
+
+	ret_clear_cqc = hikp_roce_timer_show_cqc(self);
+	if (ret_clear_cqc != 0) {
+		printf("clear cqc failed\n");
+		return ret_clear_cqc;
+	}
+
+	ret_clear_qpc = hikp_roce_timer_show_qpc(self);
+	if (ret_clear_qpc != 0) {
+		printf("clear qpc failed\n");
+		return ret_clear_qpc;
+	}
+
+	return 0;
+}
+
+static void hikp_roce_timer_execute(struct major_cmd_ctrl *self)
+{
+	int (*func[])(struct major_cmd_ctrl *self) = {
+		hikp_roce_timer_show_cqc, hikp_roce_timer_show_qpc
+	};
+	char *function[] = {"show cqc", "show qpc"};
+	int i = 0;
+	int ret;
+
+	if (g_roce_timer_param.flag) {
+		ret = hikp_roce_timer_clear(self);
+	} else {
+		for (i = 0; i < HIKP_ARRAY_SIZE(func); i++) {
+			ret = func[i](self);
+			if (ret != 0)
+				break;
+		}
+	}
+
+	if (ret != 0) {
+		self->err_no = -EINVAL;
+		snprintf(self->err_str, sizeof(self->err_str),
+			 "roce_timer %s function failed\n", function[i]);
+	}
+}
+
+static void cmd_roce_timer_init(void)
+{
+	struct major_cmd_ctrl *major_cmd = get_major_cmd();
+
+	major_cmd->option_count = 0;
+	major_cmd->execute = hikp_roce_timer_execute;
+
+	cmd_option_register("-h", "--help", false, hikp_roce_timer_help);
+	cmd_option_register("-i", "--interface", true, hikp_roce_timer_target);
+	cmd_option_register("-c", "--clear", false, hikp_roce_timer_clear_set);
+}
+
+HIKP_CMD_DECLARE("roce_timer", "get or clear roce_timer registers information",
+		 cmd_roce_timer_init);

net/roce/roce_tsp/hikp_roce_tsp.c
@@ -1,0 +1,205 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include "hikp_roce_tsp.h"
+
+static struct cmd_roce_tsp_param_t g_roce_tsp_param_t = { 0 };
+static struct roce_tsp_module g_roce_tsp_module[] = {
+	TSP_HANDLE(COMMON),
+	TSP_HANDLE(TDP),
+	TSP_HANDLE(TGP_TMP),
+};
+
+static int hikp_roce_tsp_help(struct major_cmd_ctrl *self, const char *argv)
+{
+	printf("\n  Usage: %s %s\n", self->cmd_ptr->name, "-i <interface>\n");
+	printf("\n         %s\n", self->cmd_ptr->help_info);
+	printf("  Options:\n\n");
+	printf("    %s, %-25s %s\n", "-h", "--help", "display this help and exit");
+	printf("    %s, %-25s %s\n", "-i", "--interface=<interface>", "device target, e.g. eth0");
+	printf("    %s, %-25s %s\n", "-m", "--module=<module>",
+	       "this is necessary param COMMON/TDP/TGP_TMP");
+	printf("    %s, %-25s %s\n", "-c", "--clear=<clear>", "clear TDP registers");
+	printf("    %s, %-25s %s\n", "-b", "--bank=<bank>",
+	       "[option]set which bank to read. (default 0) COMMON :  0~7\n TGP_TMP:  0~3\n ");
+	printf("\n");
+
+	return 0;
+}
+
+static int hikp_roce_tsp_target(struct major_cmd_ctrl *self, const char *argv)
+{
+	self->err_no = tool_check_and_get_valid_bdf_id(argv, &(g_roce_tsp_param_t.target));
+	if (self->err_no != 0) {
+		snprintf(self->err_str, sizeof(self->err_str), "Unknown device %s.", argv);
+		return self->err_no;
+	}
+
+	return 0;
+}
+
+static int hikp_roce_tsp_module_select(struct major_cmd_ctrl *self, const char *argv)
+{
+	bool is_found;
+	uint32_t i;
+
+	for (i = 0; i < HIKP_ARRAY_SIZE(g_roce_tsp_module); i++) {
+		is_found = strncmp(argv, (const char *)g_roce_tsp_module[i].module_name,
+			sizeof(g_roce_tsp_module[i].module_name)) == 0;
+		if (is_found) {
+			g_roce_tsp_param_t.sub_cmd_code = g_roce_tsp_module[i].sub_cmd_code;
+			return 0;
+		}
+	}
+	snprintf(self->err_str, sizeof(self->err_str), "Invalid module param!");
+	self->err_no = -EINVAL;
+
+	return -EINVAL;
+}
+
+static int hikp_roce_tsp_bank_get(struct major_cmd_ctrl *self, const char *argv)
+{
+	uint32_t temp;
+
+	self->err_no = string_toui(argv, &temp);
+	if (self->err_no) {
+		snprintf(self->err_str, sizeof(self->err_str), "get roce_tsp bank param failed.");
+		return self->err_no;
+	}
+
+	g_roce_tsp_param_t.bank_enter_flag = 1;
+	g_roce_tsp_param_t.bank_id = temp;
+	return 0;
+}
+
+static int hikp_roce_tsp_bank_check(void)
+{
+	uint32_t temp;
+
+	temp = g_roce_tsp_param_t.bank_id;
+	switch (g_roce_tsp_param_t.sub_cmd_code) {
+	case (COMMON):
+		if ((temp > MAX_TSP_BANK_NUM) || temp < 0)
+			return -EINVAL;
+		break;
+	case (TGP_TMP):
+		if ((temp > MAX_TGP_TMP_BANK_NUM) || temp < 0)
+			return -EINVAL;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int hikp_roce_tsp_clear_set(struct major_cmd_ctrl *self, const char *argv)
+{
+	g_roce_tsp_param_t.reset_flag = 1;
+
+	return 0;
+}
+
+static int hikp_roce_tsp_clear_module_check(void)
+{
+	if (g_roce_tsp_param_t.sub_cmd_code == TDP)
+		return 0;
+
+	return -EINVAL;
+}
+
+static int hikp_roce_tsp_get_data(struct hikp_cmd_ret **cmd_ret,
+				  struct roce_tsp_req_param req_data, uint32_t sub_cmd_code)
+{
+	struct hikp_cmd_header req_header = { 0 };
+
+	if (g_roce_tsp_param_t.sub_cmd_code == 0) {
+		printf("please enter module name: -m/--modlue\n");
+		return -EINVAL;
+	}
+	hikp_cmd_init(&req_header, ROCE_MOD, GET_ROCEE_TSP_CMD, sub_cmd_code);
+	*cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+	if (*cmd_ret == NULL) {
+		printf("hikptool roce_tsp cmd_ret malloc failed\n");
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static void hikp_roce_tsp_print(uint32_t total_block_num,
+				const uint32_t *offset, const uint32_t *data)
+{
+	uint32_t i;
+
+	printf("**************TSP INFO*************\n");
+	for (i = 0; i < total_block_num; i++)
+		printf("[0x%08X] : 0x%08X\n", offset[i], data[i]);
+	printf("***********************************\n");
+}
+
+static void hikp_roce_tsp_execute(struct major_cmd_ctrl *self)
+{
+	struct roce_tsp_res_param *roce_tsp_res;
+	struct roce_tsp_req_param req_data;
+	struct hikp_cmd_ret *cmd_ret;
+	int ret;
+
+	if (g_roce_tsp_param_t.reset_flag) {
+		self->err_no = hikp_roce_tsp_clear_module_check();
+		if (self->err_no) {
+			snprintf(self->err_str, sizeof(self->err_str),
+				 "roce_tsp clear function module selection error.");
+			return;
+		}
+	}
+
+	if (g_roce_tsp_param_t.bank_enter_flag) {
+		self->err_no = hikp_roce_tsp_bank_check();
+		if (self->err_no) {
+			snprintf(self->err_str, sizeof(self->err_str), "Invalid bank number!");
+			return;
+		}
+	}
+	req_data.bdf = g_roce_tsp_param_t.target.bdf;
+	req_data.reset_flag = g_roce_tsp_param_t.reset_flag;
+	req_data.bank_id = g_roce_tsp_param_t.bank_id;
+	ret = hikp_roce_tsp_get_data(&cmd_ret, req_data, g_roce_tsp_param_t.sub_cmd_code);
+	if (ret < 0) {
+		self->err_no = ret;
+		return;
+	}
+
+	roce_tsp_res = (struct roce_tsp_res_param *)cmd_ret->rsp_data;
+	hikp_roce_tsp_print(roce_tsp_res->total_block_num,
+			    roce_tsp_res->reg_data.offset, roce_tsp_res->reg_data.data);
+
+	free(cmd_ret);
+	cmd_ret = NULL;
+}
+
+static void cmd_roce_tsp_init(void)
+{
+	struct major_cmd_ctrl *major_cmd = get_major_cmd();
+
+	major_cmd->option_count = 0;
+	major_cmd->execute = hikp_roce_tsp_execute;
+
+	cmd_option_register("-h", "--help", false, hikp_roce_tsp_help);
+	cmd_option_register("-i", "--interface", true, hikp_roce_tsp_target);
+	cmd_option_register("-m", "--module", true, hikp_roce_tsp_module_select);
+	cmd_option_register("-b", "--bank", true, hikp_roce_tsp_bank_get);
+	cmd_option_register("-c", "--clear", false, hikp_roce_tsp_clear_set);
+}
+
+HIKP_CMD_DECLARE("roce_tsp", "get or clear roce_tsp registers information", cmd_roce_tsp_init);

net/roce/roce_caep/hikp_roce_caep.h
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+#ifndef __HIKP_ROCE_CAEP_H__
+#define __HIKP_ROCE_CAEP_H__
+
+#include "hikp_net_lib.h"
+
+#define ROCE_HIKP_CAEP_REG_NUM 29
+#define PER_REG_NUM 2
+
+struct cmd_roce_caep_param_t {
+	struct tool_target target;
+};
+
+struct roce_caep_req_param {
+	struct bdf_t bdf;
+};
+
+struct roce_caep_res {
+	uint32_t offset[ROCE_HIKP_CAEP_REG_NUM];
+	uint32_t data[ROCE_HIKP_CAEP_REG_NUM];
+};
+
+struct roce_caep_res_param {
+	uint32_t total_block_num;
+	struct roce_caep_res reg_data;
+};
+
+#endif /* __HIKP_ROCE_CAEP_H__ */

commit 29404abe6bd496c89bd0fa4b9832ec9230c31116

hikptool/roce: Fix missing check of reg num responsed from FW
If the reg num responsed from a new-version FW exceeds the array
length in the old-version hikptool, an out-of-bounds access error
will occur. So add a check to ensure the reg num is not greater
than tha array length.

Fixes: 392c269be165 ("support querying RoCE module information")
Fixes: 87f8605c32d3 ("hikptool/roce: Add ext query for roce_qmm cmd")
Signed-off-by: Junxian Huang <huangjunxian6@hisilicon.com>

net/roce/roce_caep/hikp_roce_caep.c
@@ -85,7 +85,14 @@ static void hikp_roce_caep_execute_origin(struct major_cmd_ctrl *self)
 		printf("hikptool roce_caep get data failed.\n");
 		goto exec_error;
 	}
+
 	roce_caep_res = (struct roce_caep_res_param *)cmd_ret->rsp_data;
+	if (roce_caep_res->total_block_num > ROCE_HIKP_CAEP_REG_NUM) {
+		printf("version might not match, adjust the reg num to %d.\n",
+		       ROCE_HIKP_CAEP_REG_NUM);
+		roce_caep_res->total_block_num = ROCE_HIKP_CAEP_REG_NUM;
+	}
+
 	hikp_roce_caep_print(roce_caep_res->total_block_num,
 			     roce_caep_res->reg_data.offset,
 			     roce_caep_res->reg_data.data);

net/roce/roce_pkt/hikp_roce_pkt.c
@@ -87,7 +87,14 @@ static void hikp_roce_pkt_execute(struct major_cmd_ctrl *self)
 		self->err_no = ret;
 		return;
 	}
+
 	roce_pkt_res = (struct roce_pkt_res_param *)cmd_ret->rsp_data;
+	if (roce_pkt_res->total_block_num > ROCE_HIKP_PKT_REG_NUM) {
+		printf("version might not match, adjust the reg num to %d.\n",
+		       ROCE_HIKP_PKT_REG_NUM);
+		roce_pkt_res->total_block_num = ROCE_HIKP_PKT_REG_NUM;
+	}
+
 	hikp_roce_pkt_print(roce_pkt_res->total_block_num,
 			    roce_pkt_res->reg_data.offset, roce_pkt_res->reg_data.data);
 

net/roce/roce_qmm/hikp_roce_qmm.c
@@ -108,7 +108,14 @@ static void hikp_roce_qmm_execute_origin(struct major_cmd_ctrl *self)
 		printf("hikptool roce_qmm get data failed.\n");
 		goto exec_error;
 	}
+
 	roce_qmm_res = (struct roce_qmm_rsp_data *)cmd_ret->rsp_data;
+	if (roce_qmm_res->reg_num > ROCE_HIKP_QMM_REG_NUM) {
+		printf("version might not match, adjust the reg num to %d.\n",
+		       ROCE_HIKP_QMM_REG_NUM);
+		roce_qmm_res->reg_num = ROCE_HIKP_QMM_REG_NUM;
+	}
+
 	hikp_roce_qmm_print(roce_qmm_res);
 
 exec_error:

net/roce/roce_timer/hikp_roce_timer.c
@@ -76,6 +76,12 @@ static int hikp_roce_timer_show_qpc(struct major_cmd_ctrl *self)
 		goto out;
 
 	timer_rsp = (struct roce_timer_rsp_data *)(cmd_ret->rsp_data);
+	if (timer_rsp->reg_num > ROCE_HIKP_TIMER_REG_NUM) {
+		printf("version might not match, adjust the reg num to %d.\n",
+		       ROCE_HIKP_TIMER_REG_NUM);
+		timer_rsp->reg_num = ROCE_HIKP_TIMER_REG_NUM;
+	}
+
 	printf("**************QPC TIMER INFO*************\n");
 	hikp_roce_timer_print(timer_rsp);
 out:
@@ -104,6 +110,12 @@ static int hikp_roce_timer_show_cqc(struct major_cmd_ctrl *self)
 		goto out;
 
 	timer_rsp = (struct roce_timer_rsp_data *)(cmd_ret->rsp_data);
+	if (timer_rsp->reg_num > ROCE_HIKP_TIMER_REG_NUM) {
+		printf("version might not match, adjust the reg num to %d.\n",
+		       ROCE_HIKP_TIMER_REG_NUM);
+		timer_rsp->reg_num = ROCE_HIKP_TIMER_REG_NUM;
+	}
+
 	printf("**************CQC TIMER INFO*************\n");
 	hikp_roce_timer_print(timer_rsp);
 out:

net/roce/roce_tsp/hikp_roce_tsp.c
@@ -184,6 +184,12 @@ static void hikp_roce_tsp_execute(struct major_cmd_ctrl *self)
 	}
 
 	roce_tsp_res = (struct roce_tsp_res_param *)cmd_ret->rsp_data;
+	if (roce_tsp_res->total_block_num > ROCE_HIKP_TSP_REG_NUM) {
+		printf("version might not match, adjust the reg num to %d.\n",
+		       ROCE_HIKP_TSP_REG_NUM);
+		roce_tsp_res->total_block_num = ROCE_HIKP_TSP_REG_NUM;
+	}
+
 	hikp_roce_tsp_print(roce_tsp_res->total_block_num,
 			    roce_tsp_res->reg_data.offset, roce_tsp_res->reg_data.data);
 
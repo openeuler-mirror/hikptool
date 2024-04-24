openeuler-mirror/hikptool

commit e05c70e1ff25feef7c6d62df39ba5ff9460cf996

support querying NIC module information
querying information about the NIC module, including hardware
SSU/PPP/IGU_EGU/MASTER/TXDMA/BIOS_COMMON/RCB/NCSI module statistics,
firmware logs, and PF hardware resource information.

Signed-off-by: cjx_2021 <cjxzju_2014@qq.com>

net/nic/nic_info/hikp_nic_info.c
@@ -1,0 +1,317 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <unistd.h>
+#include "tool_cmd.h"
+#include "hikp_net_lib.h"
+#include "hikp_nic_info.h"
+
+static struct nic_info_param g_info_param = { 0 };
+
+static int hikp_nic_cmd_get_info_help(struct major_cmd_ctrl *self, const char *argv)
+{
+	printf("\n  Usage: %s %s\n", self->cmd_ptr->name, "-i <device>");
+	printf("\n         %s\n", self->cmd_ptr->help_info);
+	printf("\n  Options:\n\n");
+	printf("    %s, %-25s %s\n", "-h", "--help", "display this help and exit");
+	printf("    %s, %-25s %s\n", "-i", "--interface=<interface>",
+	       "device target or bdf id, e.g. eth0~7 or 0000:35:00.0");
+
+	printf("\n");
+
+	return 0;
+}
+
+static int hikp_nic_cmd_get_info_target(struct major_cmd_ctrl *self, const char *argv)
+{
+	self->err_no = tool_check_and_get_valid_bdf_id(argv, &(g_info_param.target));
+	if (self->err_no != 0) {
+		snprintf(self->err_str, sizeof(self->err_str), "Unknown device %s.", argv);
+		return self->err_no;
+	}
+	g_info_param.have_interface = true;
+
+	return 0;
+}
+
+static int hikp_nic_get_hw_info(struct hikp_cmd_ret **cmd_ret)
+{
+	struct nic_info_req_para req_data = { 0 };
+	struct hikp_cmd_header req_header = { 0 };
+
+	req_data.bdf = g_info_param.target.bdf;
+	hikp_cmd_init(&req_header, NIC_MOD, GET_CHIP_INFO_CMD, CHIP_INFO_DUMP);
+	*cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+
+	return hikp_rsp_normal_check(*cmd_ret);
+}
+
+static int hikp_nic_get_curr_die_info(void)
+{
+	struct nic_info_rsp_t *info_rsp = NULL;
+	struct hikp_cmd_ret *cmd_ret;
+	int ret;
+
+	ret = hikp_nic_get_hw_info(&cmd_ret);
+	if (ret != 0) {
+		HIKP_ERROR_PRINT("Get chip info fail.\n");
+		return ret;
+	}
+	info_rsp = (struct nic_info_rsp_t *)(cmd_ret->rsp_data);
+	g_info_param.info = *info_rsp;
+	free(cmd_ret);
+	cmd_ret = NULL;
+
+	ret = get_revision_id_by_bdf(&g_info_param.target.bdf, g_info_param.revision_id);
+	if (ret != 0) {
+		HIKP_ERROR_PRINT("Getting revision fail.\n");
+		return ret;
+	}
+	ret = get_numvfs_by_bdf(&g_info_param.target.bdf, &g_info_param.numvfs);
+	if (ret != 0)
+		HIKP_ERROR_PRINT("Getting numvfs fail, ret = %d.\n", ret);
+	ret = get_dev_name_by_bdf(&g_info_param.target.bdf, g_info_param.target.dev_name);
+	if ((ret != 0) && (ret != -ENOENT)) {
+		HIKP_ERROR_PRINT("Getting dev name fail.\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static void hikp_nic_info_print_cur_pf(const struct bdf_t *bdf)
+{
+	struct tool_target *pf_target = &g_info_param.target;
+	struct tool_target target = { 0 };
+	uint8_t pf_id = bdf->fun_id;
+	uint8_t i;
+	int ret;
+
+	printf("Current function: pf%u\n", pf_id);
+	printf("\t%-16s %s\n", "pf mode:",
+	       g_info_param.info.pf_info[pf_id].pf_mode ? "X86" : "ARM");
+	printf("\t%-16s %04x:%02x:%02x.%u\n", "bdf id:",
+	       bdf->domain, bdf->bus_id, bdf->dev_id, bdf->fun_id);
+	printf("\t%-16s %u\n", "mac id:", g_info_param.info.pf_info[pf_id].mac_id);
+	printf("\t%-16s %s\n", "mac type:",
+	       g_info_param.info.pf_info[pf_id].mac_type ? "ROH" : "ETH");
+	printf("\t%-16s %u\n", "func_num:", g_info_param.info.pf_info[pf_id].func_num);
+	printf("\t%-16s %u\n", "tqp_num:", g_info_param.info.pf_info[pf_id].tqp_num);
+	printf("\t%-16s 0x%x\n", "pf_cap_flag:", g_info_param.info.pf_info[pf_id].pf_cap_flag);
+	if (pf_target->dev_name[0] != 0) {
+		printf("\t%-16s %s\n", "dev name:", pf_target->dev_name);
+		for (i = 0; i < g_info_param.numvfs; i++) {
+			ret = get_vf_dev_info_by_pf_dev_name((const char *)pf_target->dev_name,
+							     &target, i);
+			if (ret == -ENOENT)
+				continue;
+
+			if (ret != 0) {
+				HIKP_ERROR_PRINT("Getting vf's dev name fail.\n");
+				return;
+			}
+			printf("\t    pf%u-vf%u: %s <-> %04x:%02x:%02x.%u\n", pf_id, i,
+			       target.dev_name, target.bdf.domain, target.bdf.bus_id,
+			       target.bdf.dev_id, target.bdf.fun_id);
+		}
+	}
+	printf("\n");
+}
+
+static void hikp_nic_info_print_cur_die(void)
+{
+	uint8_t i;
+
+	printf("Current die(chip%u-die%u) info:\n",
+	       g_info_param.info.chip_id, g_info_param.info.die_id);
+	printf("revision id: %s", g_info_param.revision_id);
+	printf("mac mode: %u\n", g_info_param.info.mac_mode);
+	printf("pf number: %u\n", g_info_param.info.pf_num);
+	printf("pf's capability flag: 0x%x\n", g_info_param.info.cap_flag);
+	printf("pf's attributes and capabilities:\n");
+	printf("%-16s", "pf id:");
+	for (i = 0; i < g_info_param.info.pf_num; i++)
+		printf("pf%u\t", i);
+
+	printf("\n%-16s", "pf mode:");
+	for (i = 0; i < g_info_param.info.pf_num; i++)
+		printf("%s\t", g_info_param.info.pf_info[i].pf_mode ? "X86" : "ARM");
+
+	printf("\n%-16s", "mac id:");
+	for (i = 0; i < g_info_param.info.pf_num; i++)
+		printf("mac%u\t", g_info_param.info.pf_info[i].mac_id);
+
+	printf("\n%-16s", "mac type:");
+	for (i = 0; i < g_info_param.info.pf_num; i++)
+		printf("%s\t", g_info_param.info.pf_info[i].mac_type ? "ROH" : "ETH");
+
+	printf("\n%-16s", "func num:");
+	for (i = 0; i < g_info_param.info.pf_num; i++)
+		printf("%u\t", g_info_param.info.pf_info[i].func_num);
+
+	printf("\n%-16s", "tqp num:");
+	for (i = 0; i < g_info_param.info.pf_num; i++)
+		printf("%u\t", g_info_param.info.pf_info[i].tqp_num);
+
+	printf("\n\n");
+}
+
+static bool is_bus_id_accessed(void)
+{
+	uint8_t i;
+
+	for (i = 0; i < g_info_param.accessed_die_num; i++) {
+		if (g_info_param.accessed_bus_id[i] == g_info_param.target.bdf.bus_id)
+			return true;
+	}
+	g_info_param.accessed_bus_id[i] = g_info_param.target.bdf.bus_id;
+
+	return false;
+}
+
+static int hikp_nic_get_and_print_curr_die(int sockfd, struct tool_target *target,
+					   char *revision_id, struct ifaddrs *ifa_node)
+{
+	int ret;
+
+	memset(target, 0, sizeof(struct tool_target));
+	memset(revision_id, 0, MAX_PCI_ID_LEN + 1);
+
+	if (strlen(ifa_node->ifa_name) >= IFNAMSIZ) {
+		HIKP_ERROR_PRINT("parameter of device name is too long.\n");
+		return -EINVAL;
+	}
+	strncpy(target->dev_name, ifa_node->ifa_name, sizeof(target->dev_name));
+	target->dev_name[sizeof(target->dev_name) - 1] = '\0';
+	if (!is_dev_valid_and_special(sockfd, target) ||
+		is_bus_id_accessed()) {
+		return 0;
+	}
+	ret = hikp_nic_get_curr_die_info();
+	if (ret != 0)
+		return ret;
+	hikp_nic_info_print_cur_die();
+	g_info_param.accessed_die_num++;
+
+	return 0;
+}
+
+static int hikp_nic_traverse_all_hns3_dev_and_get_info(void)
+{
+	struct tool_target *target = &g_info_param.target;
+	char *revision_id = g_info_param.revision_id;
+	struct ifaddrs *ifa_node;
+	struct ifaddrs *ifa_lst;
+	int sockfd;
+	int ret;
+
+	sockfd = hikp_net_creat_sock();
+	if (sockfd < MIN_SOCKFD) {
+		HIKP_ERROR_PRINT("creat sockfd failed, sockfd is %d.\n", sockfd);
+		return -EIO;
+	}
+	ret = getifaddrs(&ifa_lst);
+	if (ret < 0) {
+		HIKP_ERROR_PRINT("getifaddrs failed.\n");
+		goto sock_out;
+	}
+	for (ifa_node = ifa_lst; ifa_node != NULL; ifa_node = ifa_node->ifa_next) {
+		if (ifa_node->ifa_addr == NULL)
+			continue;
+
+		if (ifa_node->ifa_addr->sa_family != AF_PACKET)
+			continue;
+
+		ret = hikp_nic_get_and_print_curr_die(sockfd, target, revision_id, ifa_node);
+		if (ret != 0)
+			break;
+	}
+	freeifaddrs(ifa_lst);
+sock_out:
+	close(sockfd);
+
+	return ret;
+}
+
+static void hikp_nic_info_print_cur_vf(const struct bdf_t *bdf)
+{
+	struct tool_target target = { 0 };
+	int ret;
+
+	printf("Current function is vf:\n");
+	printf("\t%-16s %04x:%02x:%02x.%u\n", "vf bdf id:",
+	       bdf->domain, bdf->bus_id, bdf->dev_id, bdf->fun_id);
+	ret = get_dev_name_by_bdf(&g_info_param.target.bdf, g_info_param.target.dev_name);
+	if ((ret != 0) && (ret != -ENOENT)) {
+		HIKP_ERROR_PRINT("Getting dev name fail.\n");
+		return;
+	}
+	if (g_info_param.target.dev_name[0] != 0)
+		printf("\t%-16s %s\n", "vf dev name:", g_info_param.target.dev_name);
+
+	if (ret == 0) {
+		ret = get_pf_dev_info_by_vf_dev_name((const char *)g_info_param.target.dev_name,
+						     &target);
+		if ((ret != 0) && (ret != -ENOENT)) {
+			HIKP_ERROR_PRINT("Getting pf dev name fail.\n");
+			return;
+		}
+		printf("Belong to:\n");
+		printf("\t%-16s pf%u\n", "pf id:", target.bdf.fun_id);
+		printf("\t%-16s %04x:%02x:%02x.%u\n", "pf bdf id:", target.bdf.domain,
+		       target.bdf.bus_id, target.bdf.dev_id, target.bdf.fun_id);
+		printf("\t%-16s %s\n", "pf dev name:", target.dev_name);
+	}
+}
+
+static void hikp_nic_info_cmd_execute(struct major_cmd_ctrl *self)
+{
+	struct bdf_t *bdf = &g_info_param.target.bdf;
+
+	if (!g_info_param.have_interface) {
+		self->err_no = hikp_nic_traverse_all_hns3_dev_and_get_info();
+		if (self->err_no != 0)
+			snprintf(self->err_str, sizeof(self->err_str),
+				 "traverse all hns3 dev fail.");
+		return;
+	}
+	if (bdf->dev_id != 0) {
+		hikp_nic_info_print_cur_vf(&g_info_param.target.bdf);
+		return;
+	}
+	self->err_no = hikp_nic_get_curr_die_info();
+	if (self->err_no != 0) {
+		snprintf(self->err_str, sizeof(self->err_str), "get current die info fail.");
+		return;
+	}
+	hikp_nic_info_print_cur_pf(&g_info_param.target.bdf);
+	hikp_nic_info_print_cur_die();
+}
+
+static void cmd_nic_get_info_init(void)
+{
+	struct major_cmd_ctrl *major_cmd = get_major_cmd();
+
+	major_cmd->option_count = 0;
+	major_cmd->execute = hikp_nic_info_cmd_execute;
+
+	cmd_option_register("-h", "--help", false, hikp_nic_cmd_get_info_help);
+	cmd_option_register("-i", "--interface", true, hikp_nic_cmd_get_info_target);
+}
+
+HIKP_CMD_DECLARE("nic_info", "show basic info of network!", cmd_nic_get_info_init);

net/nic/nic_info/hikp_nic_info.h
@@ -1,0 +1,73 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+#ifndef __HIKP_NIC_INFO_H__
+#define __HIKP_NIC_INFO_H__
+
+#include "hikp_net_lib.h"
+
+#define MAX_DIE_NUM 32
+
+struct nic_info_req_para {
+	struct bdf_t bdf;
+};
+
+struct nic_info_pf_info {
+	uint8_t pf_mode;
+	uint8_t mac_id;
+	uint8_t mac_type;
+	uint8_t rsv1;
+	uint16_t func_num;
+	uint16_t tqp_num;
+	/**********************************************
+	 * pf_cap_flag:
+	 *     bit0 == ncsi enable status
+	 *     bit1~bit31 == reserved
+	 **********************************************/
+	uint32_t pf_cap_flag;
+	uint32_t rsvd[3];
+};
+
+enum nic_info_cmd_type {
+	CHIP_INFO_DUMP = 0,
+};
+
+#define NIC_INFO_RSVD_NUM 10
+struct nic_info_rsp_t {
+	uint8_t mac_mode;
+	uint8_t chip_id;
+	uint8_t die_id;
+	uint8_t pf_num;
+	/**********************************************
+	 * cap_flag:
+	 *     bit0 == sgpio enable status
+	 *     bit1 == tm enable status
+	 *     bit2 == torus enable status
+	 *     bit3~bit31 == reserved
+	 **********************************************/
+	uint32_t cap_flag;
+	uint32_t rsv1[NIC_INFO_RSVD_NUM];
+	struct nic_info_pf_info pf_info[HIKP_MAX_PF_NUM];
+};
+
+struct nic_info_param {
+	struct tool_target target;
+	struct nic_info_rsp_t info;
+	char revision_id[MAX_PCI_ID_LEN + 1];
+	uint8_t accessed_bus_id[MAX_DIE_NUM];
+	uint8_t accessed_die_num;
+	uint8_t numvfs;
+	bool have_interface;
+};
+
+#endif

net/nic/nic_dfx/hikp_nic_dfx.c
@@ -1,0 +1,365 @@
+/*
+ * Copyright (c) 2022 Hisilicon Technologies Co., Ltd.
+ * Hikptool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <unistd.h>
+#include "tool_cmd.h"
+#include "hikp_net_lib.h"
+#include "hikp_nic_dfx.h"
+
+struct nic_dfx_param g_dfx_param = { 0 };
+
+static const struct dfx_module_cmd g_dfx_module_parse[] = {
+	{"SSU", SSU_DFX_REG_DUMP},
+	{"IGU_EGU", IGU_EGU_DFX_REG_DUMP},
+	{"PPP", PPP_DFX_REG_DUMP},
+	{"NCSI", NCSI_DFX_REG_DUMP},
+	{"BIOS", BIOS_COMM_DFX_REG_DUMP},
+	{"RCB", RCB_DFX_REG_DUMP},
+	{"TXDMA", TXDMA_DFX_REG_DUMP},
+	{"MASTER", MASTER_DFX_REG_DUMP},
+};
+
+static const struct dfx_type_parse g_dfx_type_parse[] = {
+	{INCORRECT_REG_TYPE, WIDTH_32_BIT, "INCORRECT TYPE"},
+	{TYPE_32_RX_ERROR_STATS, WIDTH_32_BIT, "32 bit RX error statistics"},
+	{TYPE_32_RX_DROP_STATS, WIDTH_32_BIT, "32 bit RX drop statistics"},
+	{TYPE_32_RX_NORMAL_STATS, WIDTH_32_BIT, "32 bit RX statistics"},
+	{TYPE_32_TX_ERROR_STATS, WIDTH_32_BIT, "32 bit TX error statistics"},
+	{TYPE_32_TX_DROP_STATS, WIDTH_32_BIT, "32 bit TX drop statistics"},
+	{TYPE_32_TX_NORMAL_STATS, WIDTH_32_BIT, "32 bit TX statistics"},
+	{TYPE_32_ERROR_STATUS, WIDTH_32_BIT, "32 bit error status"},
+	{TYPE_32_RUNNING_STATUS, WIDTH_32_BIT, "32 bit running status"},
+	{TYPE_32_CFG_STATUS, WIDTH_32_BIT, "32 bit config status"},
+	{TYPE_32_RX_PORT_ERROR_STATS, WIDTH_32_BIT, "32 bit RX port error statistics"},
+	{TYPE_32_RX_PORT_DROP_STATS, WIDTH_32_BIT, "32 bit RX port drop statistics"},
+	{TYPE_32_RX_PORT_NORMAL_STATS, WIDTH_32_BIT, "32 bit RX port statistics"},
+	{TYPE_32_TX_PORT_ERROR_STATS, WIDTH_32_BIT, "32 bit TX port error statistics"},
+	{TYPE_32_TX_PORT_DROP_STATS, WIDTH_32_BIT, "32 bit TX port drop statistics"},
+	{TYPE_32_TX_PORT_NORMAL_STATS, WIDTH_32_BIT, "32 bit TX port statistics"},
+	{TYPE_32_PORT_ERROR_STATUS, WIDTH_32_BIT, "32 bit port error status"},
+	{TYPE_32_PORT_RUNNING_STATUS, WIDTH_32_BIT, "32 bit port running status"},
+	{TYPE_32_PORT_CFG_STATUS, WIDTH_32_BIT, "32 bit port config status"},
+	{TYPE_32_COMM_STATS, WIDTH_32_BIT, "32 bit common statistics"},
+	{TYPE_32_COMM_DROP_STATS, WIDTH_32_BIT, "32 bit common drop statistics"},
+	{TYPE_32_COMM_ERROR_STATS, WIDTH_32_BIT, "32 bit common error statistics"},
+	{TYPE_64_RX_ERROR_STATS, WIDTH_64_BIT, "64 bit RX error statistics"},
+	{TYPE_64_RX_DROP_STATS, WIDTH_64_BIT, "64 bit RX drop statistics"},
+	{TYPE_64_RX_NORMAL_STATS, WIDTH_64_BIT, "64 bit RX statistics"},
+	{TYPE_64_TX_ERROR_STATS, WIDTH_64_BIT, "64 bit TX error statistics"},
+	{TYPE_64_TX_DROP_STATS, WIDTH_64_BIT, "64 bit TX drop statistics"},
+	{TYPE_64_TX_NORMAL_STATS, WIDTH_64_BIT, "64 bit TX statistics"},
+	{TYPE_64_RX_PORT_ERROR_STATS, WIDTH_64_BIT, "64 bit RX port error statistics"},
+	{TYPE_64_RX_PORT_DROP_STATS, WIDTH_64_BIT, "64 bit RX port drop statistics"},
+	{TYPE_64_RX_PORT_NORMAL_STATS, WIDTH_64_BIT, "64 bit RX port statistics"},
+	{TYPE_64_TX_PORT_ERROR_STATS, WIDTH_64_BIT, "64 bit TX port error statistics"},
+	{TYPE_64_TX_PORT_DROP_STATS, WIDTH_64_BIT, "64 bit TX port drop statistics"},
+	{TYPE_64_TX_PORT_NORMAL_STATS, WIDTH_64_BIT, "64 bit TX port statistics"},
+	{TYPE_64_COMM_STATS, WIDTH_64_BIT, "64 bit common statistics"},
+	{TYPE_64_COMM_DROP_STATS, WIDTH_64_BIT, "64 bit common drop statistics"},
+	{TYPE_64_COMM_ERROR_STATS, WIDTH_64_BIT, "64 bit common error statistics"},
+	{TYPE_64_TX_PF_ERROR_STATS, WIDTH_64_BIT, "64 bit TX pf error statistics"},
+	{TYPE_64_TX_PF_DROP_STATS, WIDTH_64_BIT, "64 bit TX pf drop statistics"},
+	{TYPE_64_TX_PF_NORMAL_STATS, WIDTH_64_BIT, "64 bit TX pf statistics"},
+};
+
+static void dfx_help_info(const struct major_cmd_ctrl *self)
+{
+	printf("\n  Usage: %s %s\n", self->cmd_ptr->name, "-i <interface>\n");
+	printf("\n         %s\n", self->cmd_ptr->help_info);
+	printf("  Options:\n\n");
+	printf("    %s, %-25s %s\n", "-h", "--help", "display this help and exit");
+	printf("    %s, %-25s %s\n", "-i", "--interface=<interface>", "device target, e.g. eth0~7");
+	printf("    %s\n", "	[-m/--module SSU/IGU_EGU/PPP/NCSI/BIOS/RCB/TXDMA/MASTER] :"
+	       "this is necessary param\n");
+}
+
+static int hikp_cmd_dfx_help(struct major_cmd_ctrl *self, const char *argv)
+{
+	dfx_help_info(self);
+	return 0;
+}
+
+static int hikp_nic_cmd_dfx_target(struct major_cmd_ctrl *self, const char *argv)
+{
+	self->err_no = tool_check_and_get_valid_bdf_id(argv, &(g_dfx_param.target));
+	if (self->err_no != 0) {
+		snprintf(self->err_str, sizeof(self->err_str), "Unknown device %s.", argv);
+		return self->err_no;
+	}
+
+	return 0;
+}
+
+static int hikp_nic_dfx_get_blk(struct hikp_cmd_ret **cmd_ret,
+				uint32_t blk_id, uint32_t sub_cmd_code)
+{
+	struct nic_dfx_req_para req_data = { 0 };
+	struct hikp_cmd_header req_header = { 0 };
+
+	req_data.bdf = g_dfx_param.target.bdf;
+	req_data.block_id = blk_id;
+	hikp_cmd_init(&req_header, NIC_MOD, GET_DFX_INFO_CMD, sub_cmd_code);
+	*cmd_ret = hikp_cmd_alloc(&req_header, &req_data, sizeof(req_data));
+
+	return hikp_rsp_normal_check(*cmd_ret);
+}
+
+static int hikp_nic_get_first_blk_dfx(struct nic_dfx_rsp_head_t *rsp_head, uint32_t **reg_data,
+				      uint32_t *max_dfx_size, uint32_t *version)
+{
+	struct nic_dfx_rsp_t *dfx_rsp = NULL;
+	struct hikp_cmd_ret *cmd_ret;
+	int ret;
+
+	ret = hikp_nic_dfx_get_blk(&cmd_ret, 0, g_dfx_param.sub_cmd_code);
+	if (ret < 0)
+		return ret;
+
+	dfx_rsp = (struct nic_dfx_rsp_t *)(cmd_ret->rsp_data);
+	*version = cmd_ret->version;
+	*rsp_head = dfx_rsp->rsp_head;
+	if (rsp_head->total_blk_num == 0) {
+		/* if total block number is zero, set total type number to zero anyway */
+		rsp_head->total_type_num = 0;
+		goto err_out;
+	}
+	*max_dfx_size = (uint32_t)(rsp_head->total_blk_num * MAX_DFX_DATA_NUM * sizeof(uint32_t));
+	*reg_data = (uint32_t *)calloc(1, *max_dfx_size);
+	if (*reg_data == NULL) {
+		HIKP_ERROR_PRINT("malloc log memory 0x%x failed.\n", *max_dfx_size);
+		ret = -ENOMEM;
+		goto err_out;
+	}
+
+	if (rsp_head->cur_blk_size > *max_dfx_size) {
+		free(*reg_data);
+		*reg_data = NULL;
+		HIKP_ERROR_PRINT("blk0 reg_data copy size error, data size: 0x%x, max size: 0x%x\n",
+				 rsp_head->cur_blk_size, *max_dfx_size);
+		ret = -EINVAL;
+		goto err_out;
+	}
+	memcpy(*reg_data, dfx_rsp->reg_data, rsp_head->cur_blk_size);
+
+	*max_dfx_size -= (uint32_t)rsp_head->cur_blk_size;
+err_out:
+	free(cmd_ret);
+	cmd_ret = NULL;
+
+	return ret;
+}
+
+static int hikp_nic_get_blk_dfx(struct nic_dfx_rsp_head_t *rsp_head, uint32_t blk_id,
+				uint32_t *reg_data, uint32_t *max_dfx_size)
+{
+	struct nic_dfx_rsp_t *dfx_rsp = NULL;
+	struct hikp_cmd_ret *cmd_ret;
+	int ret;
+
+	ret = hikp_nic_dfx_get_blk(&cmd_ret, blk_id, g_dfx_param.sub_cmd_code);
+	if (ret < 0)
+		return ret;
+
+	dfx_rsp = (struct nic_dfx_rsp_t *)(cmd_ret->rsp_data);
+	*rsp_head = dfx_rsp->rsp_head;
+	if (rsp_head->cur_blk_size > *max_dfx_size) {
+		HIKP_ERROR_PRINT("blk%u reg_data copy size error, "
+				 "data size: 0x%x, max size: 0x%x\n",
+				 blk_id, rsp_head->cur_blk_size, *max_dfx_size);
+		free(cmd_ret);
+		cmd_ret = NULL;
+		return -EINVAL;
+	}
+	memcpy(reg_data, dfx_rsp->reg_data, rsp_head->cur_blk_size);
+	*max_dfx_size -= (uint32_t)rsp_head->cur_blk_size;
+	free(cmd_ret);
+	cmd_ret = NULL;
+
+	return 0;
+}
+
+static int cmd_dfx_module_select(struct major_cmd_ctrl *self, const char *argv)
+{
+	size_t arr_size = HIKP_ARRAY_SIZE(g_dfx_module_parse);
+	bool is_found;
+	size_t i;
+
+	for (i = 0; i < arr_size; i++) {
+		is_found = strncmp(argv, (const char *)g_dfx_module_parse[i].module_name,
+						   sizeof(g_dfx_module_parse[i].module_name)) == 0;
+		if (is_found) {
+			g_dfx_param.sub_cmd_code = g_dfx_module_parse[i].sub_cmd_code;
+			g_dfx_param.module_idx = i;
+			g_dfx_param.flag |= MODULE_SET_FLAG;
+			return 0;
+		}
+	}
+	dfx_help_info(self);
+	snprintf(self->err_str, sizeof(self->err_str), "-m/--module param error!!!");
+	self->err_no = -EINVAL;
+
+	return -EINVAL;
+}
+
+static void hikp_nic_dfx_print_b32(uint32_t num, uint32_t *reg_data)
+{
+	uint32_t word_num = num * WORD_NUM_PER_REG;
+	uint16_t offset;
+	uint32_t value;
+	uint32_t index;
+	uint32_t i;
+
+	for (i = 0, index = 1; i < word_num; i = i + WORD_NUM_PER_REG, index++) {
+		offset = (uint16_t)HI_GET_BITFIELD(reg_data[i], 0, DFX_REG_ADDR_MASK);
+		value = reg_data[i + 1];
+		printf("%03u: 0x%04x\t0x%08x\n", index, offset, value);
+	}
+}
+
+static void hikp_nic_dfx_print_b64(uint32_t num, uint32_t *reg_data)
+{
+	uint32_t word_num = num * WORD_NUM_PER_REG;
+	uint16_t offset;
+	uint64_t value;
+	uint32_t index;
+	uint32_t i;
+
+	for (i = 0, index = 1; i < word_num; i = i + WORD_NUM_PER_REG, index++) {
+		offset = (uint16_t)HI_GET_BITFIELD(reg_data[i], 0, DFX_REG_ADDR_MASK);
+		value = (uint64_t)reg_data[i + 1] |
+			(HI_GET_BITFIELD((uint64_t)reg_data[i], DFX_REG_VALUE_OFF,
+			DFX_REG_VALUE_MASK) << BIT_NUM_OF_WORD);
+		printf("%03u: 0x%04x\t0x%016lx\n", index, offset, value);
+	}
+}
+
+static bool is_type_found(uint16_t type_id, uint32_t *index)
+{
+	size_t arr_size = HIKP_ARRAY_SIZE(g_dfx_type_parse);
+	size_t i;
+
+	for (i = 0; i < arr_size; i++) {
+		if (g_dfx_type_parse[i].type_id == type_id) {
+			*index = i;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+static void hikp_nic_dfx_print_type_head(uint8_t type_id, uint8_t *last_type_id)
+{
+	uint32_t index = 0;
+
+	if (type_id != *last_type_id) {
+		printf("-----------------------------------------------------\n");
+		if (is_type_found(type_id, &index))
+			printf("type name: %s\n\n", g_dfx_type_parse[index].type_name);
+		else
+			HIKP_WARN_PRINT("type name: unknown type, type id is %u\n\n", type_id);
+
+		*last_type_id = type_id;
+	}
+}
+
+static void hikp_nic_dfx_print(const struct nic_dfx_rsp_head_t *rsp_head, uint32_t *reg_data)
+{
+	struct nic_dfx_type_head *type_head;
+	uint8_t last_type_id = 0;
+	uint32_t *ptr = reg_data;
+	uint8_t i;
+
+	printf("****************** module %s reg dump start ********************\n",
+		g_dfx_module_parse[g_dfx_param.module_idx].module_name);
+	for (i = 0; i < rsp_head->total_type_num; i++) {
+		type_head = (struct nic_dfx_type_head *)ptr;
+		if (type_head->type_id == INCORRECT_REG_TYPE) {
+			HIKP_ERROR_PRINT("No.%u type is incorrect reg type\n", i + 1u);
+			break;
+		}
+		hikp_nic_dfx_print_type_head(type_head->type_id, &last_type_id);
+		ptr++;
+		if (type_head->bit_width == WIDTH_32_BIT) {
+			hikp_nic_dfx_print_b32((uint32_t)type_head->reg_num, ptr);
+		} else if (type_head->bit_width == WIDTH_64_BIT) {
+			hikp_nic_dfx_print_b64((uint32_t)type_head->reg_num, ptr);
+		} else {
+			HIKP_ERROR_PRINT("type%u's bit width error.\n", type_head->type_id);
+			break;
+		}
+		ptr += (uint32_t)type_head->reg_num * WORD_NUM_PER_REG;
+	}
+	printf("################### ====== dump end ====== ######################\n");
+}
+
+static void hikp_nic_dfx_cmd_execute(struct major_cmd_ctrl *self)
+{
+	struct nic_dfx_rsp_head_t rsp_head = { 0 };
+	struct nic_dfx_rsp_head_t tmp_head = { 0 };
+	uint32_t *reg_data = NULL;
+	uint32_t real_reg_size;
+	uint32_t max_dfx_size;
+	uint32_t version;
+	uint32_t i;
+	int ret;
+
+	if (!(g_dfx_param.flag & MODULE_SET_FLAG)) {
+		self->err_no = -EINVAL;
+		snprintf(self->err_str, sizeof(self->err_str), "Please specify a module.");
+		dfx_help_info(self);
+		return;
+	}
+	self->err_no = hikp_nic_get_first_blk_dfx(&rsp_head, &reg_data, &max_dfx_size, &version);
+	if (self->err_no != 0) {
+		snprintf(self->err_str, sizeof(self->err_str), "get the first block dfx fail.");
+		return;
+	}
+	real_reg_size = (uint32_t)rsp_head.cur_blk_size;
+	for (i = 1; i < rsp_head.total_blk_num; i++) {
+		self->err_no = hikp_nic_get_blk_dfx(&tmp_head, i,
+						    reg_data + (real_reg_size / sizeof(uint32_t)),
+						    &max_dfx_size);
+		if (self->err_no != 0) {
+			snprintf(self->err_str, sizeof(self->err_str),
+				 "getting block%u reg fail.", i);
+			free(reg_data);
+			return;
+		}
+		real_reg_size += (uint32_t)tmp_head.cur_blk_size;
+		memset(&tmp_head, 0, sizeof(struct nic_dfx_rsp_head_t));
+	}
+
+	printf("DFX cmd version: 0x%x\n\n", version);
+	hikp_nic_dfx_print((const struct nic_dfx_rsp_head_t *)&rsp_head, reg_data);
+	free(reg_data);
+}
+
+static void cmd_nic_dfx_init(void)
+{
+	struct major_cmd_ctrl *major_cmd = get_major_cmd();
+
+	major_cmd->option_count = 0;
+	major_cmd->execute = hikp_nic_dfx_cmd_execute;
+
+	cmd_option_register("-h", "--help", false, hikp_cmd_dfx_help);
+	cmd_option_register("-i", "--interface", true, hikp_nic_cmd_dfx_target);
+	cmd_option_register("-m", "--module", true, cmd_dfx_module_select);
+}
+
+HIKP_CMD_DECLARE("nic_dfx", "dump dfx info of hardware", cmd_nic_dfx_init);

commit 7192153bc92f24f24531235e560df08e3e4264d7

hikptool: Fixed an issue where the MAC type is incorrectly displayed
Currently, only the ETH and ROH types can be displayed, add the UB
mac type to be displayed

Fixes: e05c70e1ff25 ("support querying NIC module information")
Signed-off-by: z00470624 <zhuweijia@huawei.com>

net/nic/nic_info/hikp_nic_info.c
@@ -98,6 +98,13 @@ static int hikp_nic_get_curr_die_info(void)
 	return 0;
 }
 
+static const char *hikp_nic_info_get_mac_type(uint8_t type_val)
+{
+	const char *mac_type[MAC_TYPE_MAX] = {"ETH", "ROH", "UB"};
+
+	return (type_val >= MAC_TYPE_MAX) ? "UNKNOWN" : mac_type[type_val];
+}
+
 static void hikp_nic_info_print_cur_pf(const struct bdf_t *bdf)
 {
 	struct tool_target *pf_target = &g_info_param.target;
@@ -113,7 +120,7 @@ static void hikp_nic_info_print_cur_pf(const struct bdf_t *bdf)
 	       bdf->domain, bdf->bus_id, bdf->dev_id, bdf->fun_id);
 	printf("\t%-16s %u\n", "mac id:", g_info_param.info.pf_info[pf_id].mac_id);
 	printf("\t%-16s %s\n", "mac type:",
-	       g_info_param.info.pf_info[pf_id].mac_type ? "ROH" : "ETH");
+	       hikp_nic_info_get_mac_type(g_info_param.info.pf_info[pf_id].mac_type));
 	printf("\t%-16s %u\n", "func_num:", g_info_param.info.pf_info[pf_id].func_num);
 	printf("\t%-16s %u\n", "tqp_num:", g_info_param.info.pf_info[pf_id].tqp_num);
 	printf("\t%-16s 0x%x\n", "pf_cap_flag:", g_info_param.info.pf_info[pf_id].pf_cap_flag);
@@ -162,7 +169,7 @@ static void hikp_nic_info_print_cur_die(void)
 
 	printf("\n%-16s", "mac type:");
 	for (i = 0; i < g_info_param.info.pf_num; i++)
-		printf("%s\t", g_info_param.info.pf_info[i].mac_type ? "ROH" : "ETH");
+		printf("%s\t", hikp_nic_info_get_mac_type(g_info_param.info.pf_info[i].mac_type));
 
 	printf("\n%-16s", "func num:");
 	for (i = 0; i < g_info_param.info.pf_num; i++)

net/nic/nic_info/hikp_nic_info.h
@@ -70,4 +70,11 @@ struct nic_info_param {
 	bool have_interface;
 };
 
+enum nic_info_mac_type {
+	MAC_TYPE_ETH = 0,
+	MAC_TYPE_ROH,
+	MAC_TYPE_UB,
+	MAC_TYPE_MAX,
+};
+
 #endif
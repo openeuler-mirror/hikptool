openeuler-mirror/hikptool

commit 1d2a0bb9cddc8b099540fe3eb52037f644886fe1

hikptool : Querying the SerDes Relationship of the PCIe
This function is used to query the macro corresponding to the
port of the PCIe. It is a type of SerDes multiplexing relationship.

pcie/func_lib/pcie_func/pcie_statistics.c
@@ -25,6 +25,10 @@ static const char *g_global_width_name[GLOBAL_WIDTH_TABLE_SIZE] = {
 	"PCIE_WIDTH_X1", "PCIE_WIDTH_X2", "PCIE_WIDTH_X4", "PCIE_WIDTH_X8", "PCIE_WIDTH_X16"
 };
 
+static const char *g_global_ndie_name[] = {
+	"Ndie_A", "Ndie_B"
+};
+
 static int port_distribution_rsp_data_check(const struct hikp_cmd_ret *cmd_ret, uint32_t *port_num)
 {
 	size_t rsp_data_size, expect_data_size;
@@ -58,6 +62,29 @@ static int port_distribution_rsp_data_check(const struct hikp_cmd_ret *cmd_ret, 
 	return 0;
 }
 
+static int pcie_portid_serdes_relation(const struct pcie_macro_info *macro_info,
+					uint32_t macro_num, uint32_t ndie_id)
+{
+	uint32_t i, j;
+
+	if (ndie_id >= HIKP_ARRAY_SIZE(g_global_ndie_name)) {
+		Info("PCIe Base", "ndie_id [%u]: %s\n", ndie_id, "UNKNOWN_NDIE");
+		return -1;
+	}
+
+	if (macro_num >= MAX_MACRO_ONEPORT) {
+		Info("PCIe Base", "macro_num [%u] exceeds the maximum array length\n", macro_num);
+		return -1;
+	}
+
+	Info("PCIe Base", "\tndie_id: %s\n", g_global_ndie_name[ndie_id]);
+	for (i = 0; i < macro_num; i++) {
+		for (j = macro_info[i].lane_s; j <= macro_info[i].lane_e; j++)
+			Info("PCIe Base", "\t\tmacro %d \t lane: %d\n", macro_info[i].id, j);
+	}
+	return 0;
+}
+
 int pcie_port_distribution_get(uint32_t chip_id)
 {
 	struct hikp_cmd_header req_header;
@@ -86,6 +113,9 @@ int pcie_port_distribution_get(uint32_t chip_id)
 		}
 		Info("PCIe Base", "port_id[%u] %s\n", port_info->info_pair[i].port_id,
 			g_global_width_name[port_info->info_pair[i].port_width]);
+		pcie_portid_serdes_relation(port_info->info_pair[i].macro_info,
+					    port_info->info_pair[i].macro_num,
+					    port_info->info_pair[i].ndie_id);
 	}
 free_cmd_ret:
 	free(cmd_ret);

pcie/func_lib/pcie_func/pcie_statistics.h
@@ -17,6 +17,7 @@
 #include "pcie_common_api.h"
 
 #define GLOBAL_WIDTH_TABLE_SIZE 5
+#define MAX_MACRO_ONEPORT 3
 
 union mac_test_cnt {
 	/* Define the struct bits */
@@ -88,9 +89,18 @@ union dfx_dcrc_err_num {
 	unsigned int u32;
 };
 
+struct pcie_macro_info {
+	uint32_t id;
+	uint32_t lane_s;
+	uint32_t lane_e;
+};
+
 struct pcie_info_distribution_pair {
 	uint32_t port_id;
 	uint32_t port_width;
+	uint32_t ndie_id;
+	uint32_t macro_num;
+	struct pcie_macro_info macro_info[MAX_MACRO_ONEPORT];
 };
 
 struct pcie_port_info {

commit 23e8ac5f3bf9b8b3b95d949b7ed2ac76bfe7bed2

hikptool : augument serdes relationship of the PCIe
The Ndie infomation is missing when different chips are adapted.
Add two Ndie to ensure that die information is properly printed.

Fixes: 1d2a0bb9cddc ("hikptool : Querying the SerDes Relationship of the PCIe")
Signed-off-by: m30045533 <moubingquan@huawei.com>

pcie/func_lib/pcie_func/pcie_statistics.c
@@ -26,7 +26,7 @@ static const char *g_global_width_name[GLOBAL_WIDTH_TABLE_SIZE] = {
 };
 
 static const char *g_global_ndie_name[] = {
-	"Ndie_A", "Ndie_B"
+	"Ndie_A", "Ndie_B", "Ndie_C", "Ndie_D"
 };
 
 static int port_distribution_rsp_data_check(const struct hikp_cmd_ret *cmd_ret, uint32_t *port_num)